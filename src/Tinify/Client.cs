a00a67260e5847257e41e847762647cb235a1423
Remco <me@remcokoopmans.com>
2025-07-02 09:58:17 +0200

    Update Client.cs

--- src/Tinify/Client.cs
+++ src/Tinify/Client.cs
@@ -7,6 +7,7 @@ using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 
 namespace TinifyAPI
 {
